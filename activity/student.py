979c6980ba2d5c57f86fe2454af676787ae95449
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-03-27 12:27:07 -0700

    Add capitalize to the name

--- activity/student.py
+++ activity/student.py
@@ -5,8 +5,9 @@ class Student:
     self.grade = grade
     self.courses = courses
   
-  def add_class(self, class_name):
-    self.courses.append(class_name)
+  def add_class(self, class_to_add):
+    self.courses.append(class_to_add)
+
     return self.courses
   
   def get_num_classes(self):
@@ -14,6 +15,6 @@ class Student:
 
   def summary(self):
     num_courses = self.get_num_classes()
-    print(f"{self.name} is a {self.grade} enrolled in {num_courses} classes")
+    print(f"{self.name.capitalize()} is a {self.grade} enrolled in {num_courses} classes")
 
 
