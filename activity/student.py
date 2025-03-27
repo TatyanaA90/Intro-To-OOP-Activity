e7bf2f2d151fa7dd57cf0c76bdc3950fb0aea5c0
dehuihu <dehuihu@gmail.com>
2025-03-27 11:21:56 -0700

    Implemented Student class

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,19 @@
 # add your Student class here!
+class Student:
+  def __init__(self, name, year_in_college, courses):
+    self.name = name
+    self.year_in_college = year_in_college
+    self.courses = courses
+  
+  def add_class(self, class_name):
+    self.courses.append(class_name)
+    return self.courses
+  
+  def get_num_classes(self):
+    return len(self.courses)
+
+  def summary(self):
+    num_courses = self.get_num_classes()
+    print(f"{self.name} is a {self.year_in_college} enrolled in {num_courses} classes")
+
+
